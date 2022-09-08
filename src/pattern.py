7bdc981d2cb147349d6886b65fd7feee89866f8a
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-09-08 14:59:04 +0200

    nr1

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,20 @@
 
 # Print the pattern
+# n=0
+# while n<=5:
+#     ast
+#     print()
+
+n=5
+string=''
+for i in range(n):
+    string+='*'
+    print(string)
+    string+=' '
+for i in range(1,n):
+    times=n-i
+    string=('*'+' ')*(times-1)+'*'
+    print(string)
+
+
+#make a list of 4 '*' and remove last index every iteration
